4c694cf5c2dd6a0aedac3e36f39c6c56bab70cc2
Zak Wilson <zak.wilson@gmail.com>
2011-01-21 02:47:12 +0100

    back to 1.2

--- map.clj
+++ map.clj
@@ -6,8 +6,7 @@
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 
-(ns zutil.map
-  (:use clojure.contrib.math))
+(ns zutil.map)
 
 (defn lookup-with [default k a-map]
   (let [found (a-map k)]
