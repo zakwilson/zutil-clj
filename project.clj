255e954164e45dd525cd8ce7e51eeb21c5f47995
Zak Wilson <zak.wilson@gmail.com>
2011-01-27 02:35:57 +0100

    leiningenified

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject zutil "0.1.0"
+  :dependencies  [[org.clojure/clojure "1.2.0"]
+                  [org.clojure/clojure-contrib "1.2.0"]
+                  ]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
